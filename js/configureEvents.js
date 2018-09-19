13aa705dcb732ade8e31004a40fe74b874c1f039
Maximka <tichkov_maxim@mail.ru>
2018-09-19 21:01:29 +0600

    add swiper dist, change height of nav and menu

---
+++ js/configureEvents.js
@@ -0,0 +1,28 @@
+let mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+
+});
+
+let menu = $('.sidenav');
+menu.bind("touchmove",open_close_menu);
+menu.bind("click",open_close_menu);
+$('.toggler').bind("click",open_close_menu);
+
+
+function open_close_menu() {
+    let sideNav = $('#mySidenav');
+    if (sideNav.width() !== 0) {
+        sideNav.width("0px");
+    } else {
+        sideNav.width("100vw");
+    }
+}
